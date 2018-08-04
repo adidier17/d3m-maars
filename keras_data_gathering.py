651b8179ef79b70e2db24aa079286335e02ea2ab
Bhavin Shah <bhavints@usc.edu>
2018-08-03 22:44:08 -0700

    Update keras_data_gathering.py

--- keras_data_gathering.py
+++ keras_data_gathering.py
@@ -31,44 +31,6 @@ from skopt import gp_minimize, forest_minimize
 from skopt.space import Real, Categorical, Integer
 from skopt.utils import use_named_args
 
-# Hyperparameter tuning
-dim_learning_rate = Real(low=1e-6, high=1e-2, prior='log-uniform',
-                         name='learning_rate')
-
-
-dim_num_dense_layers = Integer(low=10, high=100, name='num_dense_layers')
-
-dim_num_conv_layers = Integer(low=1, high=4, name='num_conv_layers')
-
-dim_kernel_size = Integer(low=3, high=10, name='kernel_size')
-
-dim_num_filters = Integer(low=16, high=64, name='num_filters')
-
-dimensions = [dim_learning_rate,
-              dim_num_dense_layers,
-              dim_num_conv_layers,
-              dim_kernel_size,
-              dim_num_filters]
-
-default_parameters = [1e-3, 25, 2, 5, 32]
-
-# We know that MNIST images are 128 pixels in each dimension.
-img_width = 128
-
-# Images are stored in one-dimensional arrays of this length.
-img_size_flat = img_width * img_width
-
-# Tuple with height and width of images used to reshape arrays.
-# This is used for plotting the images.
-img_shape = (img_width, img_width)
-
-# Number of colour channels for the images: 1 channel for gray-scale.
-num_channels = 0
-
-# Tuple with height, width and depth used to reshape arrays.
-# This is used for reshaping in Keras.
-img_shape_full = (img_width, img_width)
-
 
 # Set image directory, only png's
 paths = ('april/orthoimage', 'mayone/orthoimage', 'maytwo/orthoimage')
@@ -113,8 +75,8 @@ for root, dirs, files in chain.from_iterable(os.walk(path) for path in paths):
 	if (filename.endswith('png')) :
 	    p=os.path.join(root,filename)
             # Read the current image and update the previous sample id var
-            #im = imageio.imread(p)
-            #gray = rgb2gray(im)
+            im = imageio.imread(p)
+            gray = rgb2gray(im)
 	  
             # Get the sample id
             sampleid = (filename.split('_'))[0]
@@ -125,14 +87,14 @@ for root, dirs, files in chain.from_iterable(os.walk(path) for path in paths):
 	
 #		    print gray[0][0].shape
 #		    print gray[0][0]
-	            #imageList.append(gray)
+	            imageList.append(gray)
  		    correspondingTorque.append(float(csvtorque[sampleid]))
-#		    if (index > 0) :
-#	            	correspondingRoll.append(float(csvroll[sampleid]))
-#		    	correspondingPitch.append(float(csvpitch[sampleid]))
-#		    else: 
-#                	correspondingRoll.append(float(0))
-#                	correspondingPitch.append(float(0))
+		    if (index > 0) :
+	            	correspondingRoll.append(float(csvroll[sampleid]))
+		    	correspondingPitch.append(float(csvpitch[sampleid]))
+		    else: 
+	               	correspondingRoll.append(float(0))
+                	correspondingPitch.append(float(0))
 #		    if (index > 5830):
 #			print sampleid
 #			print (float(csvroll[sampleid]))
@@ -146,9 +108,9 @@ for root, dirs, files in chain.from_iterable(os.walk(path) for path in paths):
             index += 1
 
     # Convert imagelist into numpy array
-    #imageArray = np.asarray(imageList, dtype=np.float32)
+    imageArray = np.asarray(imageList, dtype=np.float32)
 
-    #np.save(imagePath, imageArray)
+    np.save(imagePath, imageArray)
 
     # Convert our torquelist to numpuy array
     torque = np.asarray(correspondingTorque, dtype=np.float32)
@@ -162,11 +124,11 @@ for root, dirs, files in chain.from_iterable(os.walk(path) for path in paths):
 
     np.save(torquePath, torque)
 
-#    roll = np.asarray(correspondingRoll, dtype=np.float32)
-#    pitch = np.asarray(correspondingPitch, dtype=np.float32)
+    roll = np.asarray(correspondingRoll, dtype=np.float32)
+    pitch = np.asarray(correspondingPitch, dtype=np.float32)
 #    print len(roll)
 #    print len(pitch)
 
-#    np.save(rollPath, roll)
-#    np.save(pitchPath, pitch)
+    np.save(rollPath, roll)
+    np.save(pitchPath, pitch)
 
